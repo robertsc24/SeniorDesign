99979595508aae5e3a3dbd59495f297185d51d25
furste <furste@msoe.edu>
2024-05-01 15:20:12 -0500

    new files

---
+++ src/CalendarPage.js
@@ -0,0 +1,8 @@
+import "./loginPage.css";
+import calendarMockup from "./images/calendarmockup.png";
+
+export default function CalendarPage() {
+    return (
+        <img style={{width: "100%", height: "100%"}} src={calendarMockup}/>
+    );
+}
---
+++ src/LoginPage.js
@@ -0,0 +1,8 @@
+import "./loginPage.css";
+import loginMockup from "./images/loginmockup.png";
+
+export default function LoginPage() {
+    return (
+        <img style={{width: "100%", height: "100%"}} src={loginMockup}/>
+    );
+}
